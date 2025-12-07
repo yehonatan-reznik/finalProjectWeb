22f2a0f5f20eee5c2b2987ff19f5ca585c402d2a
rezn1k <yehonatan.reznik@gmail.com>
2025-12-07 15:12:06 +0200

    work laser on off from web

--- js/allowedPhones.js
+++
@@ -1,8 +0,0 @@
-// Centralized list of phone numbers allowed to access the SkyShield console.
-// Update this list as needed; numbers can be in any format because the auth
-// layer normalizes by stripping non-digit characters before comparison.
-(function (window) {
-  window.SkyShieldAllowedPhones = [
-    '0584648044'
-  ];
-})(window);
